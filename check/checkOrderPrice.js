e91d091793ac082d5e088b9f9258df604544075e
LiFayi <lifayi2008@gmail.com>
2021-12-08 14:08:17 +0800

    bug fix

--- check/checkOrderPrice.js
+++ check/checkOrderPrice.js
@@ -5,8 +5,37 @@ async function checkOrderPrice() {
     let mongoClient = new MongoClient(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
     try {
         await mongoClient.connect();
-        const collection = mongoClient.db(config.dbName).collection('pasar_order');
-        return await collection.find({}).sort({blockNumber: -1}).project({"_id": 0, orderId: 1, price: 1}).toArray();
+        let collection = mongoClient.db(config.dbName).collection('pasar_order');
+        let result = await collection.find({}).sort({blockNumber: -1}).project({"_id": 0, orderId: 1, price: 1}).toArray();
+
+        collection = mongoClient.db(config.dbName).collection('pasar_order_event');
+
+        let result2 = await collection.aggregate([
+            { $sort: {blockNumber: -1}},
+            { $match: {event: {$in: ["OrderPriceChanged"]}}},
+            { $group: {_id: "$orderId", doc: {$first: "$$ROOT"}}},
+            { $replaceRoot: { newRoot: "$doc"}},
+        ]).toArray();
+
+        let a2 = new Map();
+        result2.forEach(item => {
+            let price = item.data.newPrice
+            a2.set(item.orderId, price);
+        })
+
+        let i = 0;
+        result.forEach(item => {
+            let price = a2.get(item.orderId);
+            if(price === undefined) {
+                return
+            }
+            if(item.price !== price) {
+                console.log(`${item.orderId}  pasar_order price: ${item.price} ==> pasar_event_order price: ${price}`)
+                i++
+            }
+        })
+
+        console.log(`there are ${i} orders price incorrect`)
     } catch (err) {
         logger.error(err);
     } finally {
@@ -14,36 +43,5 @@ async function checkOrderPrice() {
     }
 }
 
-checkOrderPrice().then(async result => {
-    let client = new MongoClient(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
-    await client.connect();
-    let collection = client.db(config.dbName).collection('pasar_order_event');
-
-    let result2 = await collection.aggregate([
-        { $sort: {blockNumber: -1}},
-        { $match: {event: {$in: ["OrderPriceChanged"]}}},
-        { $group: {_id: "$orderId", doc: {$first: "$$ROOT"}}},
-        { $replaceRoot: { newRoot: "$doc"}},
-    ]).toArray();
-
-    let a2 = new Map();
-    result2.forEach(item => {
-        let price = item.data.newPrice
-        a2.set(item.orderId, price);
-    })
-
-    let i = 0;
-    result.forEach(item => {
-        let price = a2.get(item.orderId);
-        if(price === undefined) {
-            return
-        }
-        if(item.price !== price) {
-            console.log(`${item.orderId}  pasar_order price: ${item.price} ==> pasar_event_order price: ${price}`)
-            i++
-        }
-    })
-
-    console.log(`there are ${i} orders price incorrect`)
-})
+checkOrderPrice().then(console.log)
 
--- check/checkOrderState.js
+++ check/checkOrderState.js
@@ -5,8 +5,33 @@ async function checkOrderState() {
     let mongoClient = new MongoClient(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
     try {
         await mongoClient.connect();
-        const collection = mongoClient.db(config.dbName).collection('pasar_order');
-        return await collection.find({}).sort({blockNumber: -1}).project({"_id": 0, orderId: 1, orderState: 1}).toArray();
+        let collection = mongoClient.db(config.dbName).collection('pasar_order');
+        let result = await collection.find({}).sort({blockNumber: -1}).project({"_id": 0, orderId: 1, orderState: 1}).toArray();
+
+        collection = client.db(config.dbName).collection('pasar_order_event');
+
+        let result2 = await collection.aggregate([
+            { $sort: {blockNumber: -1}},
+            { $match: {event: {$in: ["OrderCanceled", "OrderFilled", "OrderForSale"]}}},
+            { $group: {_id: "$orderId", doc: {$first: "$$ROOT"}}},
+            { $replaceRoot: { newRoot: "$doc"}},
+        ]).toArray();
+
+        let a2 = new Map();
+        result2.forEach(item => {
+            let state = item.event === "OrderForSale" ? "1" : item.event === "OrderFilled" ? "2" : item.event === "OrderCanceled" ? "3" : "0"
+            a2.set(item.orderId, state);
+        })
+
+        let i = 0;
+        result.forEach(item => {
+            if(item.orderState !== a2.get(item.orderId)) {
+                console.log(`${item.orderId}  pasar_order state: ${item.orderState} ==> pasar_event_order state: ${a2.get(item.orderId)}`)
+                i++
+            }
+        })
+
+        console.log(`there are ${i} orders state incorrect`)
     } catch (err) {
         logger.error(err);
     } finally {
@@ -14,32 +39,5 @@ async function checkOrderState() {
     }
 }
 
-checkOrderState().then(async result => {
-    let client = new MongoClient(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
-    await client.connect();
-    let collection = client.db(config.dbName).collection('pasar_order_event');
-
-    let result2 = await collection.aggregate([
-        { $sort: {blockNumber: -1}},
-        { $match: {event: {$in: ["OrderCanceled", "OrderFilled", "OrderForSale"]}}},
-        { $group: {_id: "$orderId", doc: {$first: "$$ROOT"}}},
-        { $replaceRoot: { newRoot: "$doc"}},
-    ]).toArray();
-
-    let a2 = new Map();
-    result2.forEach(item => {
-        let state = item.event === "OrderForSale" ? "1" : item.event === "OrderFilled" ? "2" : item.event === "OrderCanceled" ? "3" : "0"
-        a2.set(item.orderId, state);
-    })
-
-    let i = 0;
-    result.forEach(item => {
-        if(item.orderState !== a2.get(item.orderId)) {
-            console.log(`${item.orderId}  pasar_order state: ${item.orderState} ==> pasar_event_order state: ${a2.get(item.orderId)}`)
-            i++
-        }
-    })
-
-    console.log(`there are ${i} orders state incorrect`)
-})
+checkOrderState().then(console.log)
 
