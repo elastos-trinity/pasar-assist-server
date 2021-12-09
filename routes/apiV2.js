e4ff79a1cc2cf45e73f02aa4e76805740458cbfc
LiFayi <lifayi2008@gmail.com>
2021-12-09 17:20:19 +0800

    v2 api

---
+++ routes/apiV2.js
@@ -0,0 +1,84 @@
+let express = require('express');
+let router = express.Router();
+let apiV2DBService = require('../service/apiV2DBService');
+const BigNumber = require('bignumber.js');
+
+router.get('/collectible/tokenId/:tokenId', function (res, req) {
+    return apiV2DBService.queryCollectibleByTokenId(req.params.tokenId)
+})
+
+router.get('/collectibles/creator/:creator', function (res, req) {
+    return apiV2DBService.queryCollectibleByOther('creator', req.params.creator)
+})
+
+router.get('/collectibles/owner/:owner', function (res, req) {
+    return apiV2DBService.queryCollectibleByOther('owner', req.params.owner)
+})
+
+router.get('/collectibles/keyword/:keyword', function (res, req) {
+    return apiV2DBService.queryCollectibleByOther('keyword', req.params.keyword)
+})
+
+router.get('/collectibles/all', function (res, req) {
+    return apiV2DBService.queryAllCollectibles()
+})
+
+router.get('/creators/all', function (res, req) {
+    return apiV2DBService.queryAllCreators()
+})
+
+router.get('/orders/filled/tokenId/:tokenId', function (res, req) {
+    return apiV2DBService.queryOrders('2', 'tokenId', req.params.tokenId)
+})
+
+router.get('/orders/filled/seller/:seller', function (res, req) {
+    return apiV2DBService.queryOrdersBySeller('2',  req.params.seller)
+})
+
+router.get('/orders/filled/buyer/:buyer', function (res, req) {
+    return apiV2DBService.queryOrders('2', 'buyer', req.params.buyer)
+})
+
+router.get('/orders/filled/keyword/:keyword', function (res, req) {
+    return apiV2DBService.queryOrdersByKeyword('2', 'keyword', req.params.keyword)
+})
+
+router.get('/orders/filled/all', function (res, req) {
+    return apiV2DBService.queryAllOrder('2')
+})
+
+router.get('/orders/canceled/tokenId/:tokenId', function (res, req) {
+    return apiV2DBService.queryOrders('3', 'tokenId', req.params.tokenId)
+})
+
+router.get('/orders/canceled/seller/:seller', function (res, req) {
+    return apiV2DBService.queryOrdersBySeller('3', req.params.seller)
+})
+
+router.get('/orders/canceled/all', function (res, req) {
+    return apiV2DBService.queryAllOrder('3')
+})
+
+router.get('/orders/sale/tokenId/:tokenId', function (res, req) {
+    return apiV2DBService.queryOrders('1', 'tokenId', req.params.tokenId)
+})
+
+router.get('/orders/sale/seller/:seller', function (res, req) {
+    return apiV2DBService.queryOrdersBySeller('1', req.params.seller)
+})
+
+router.get('/orders/sale/all', function (res, req) {
+    return apiV2DBService.queryAllOrder('1')
+})
+
+router.get('/orders/priceChanged/tokenId/:tokenId', function (res, req) {
+
+})
+
+router.get('/orders/priceChanged/seller/:seller', function (res, req) {
+
+})
+
+
+
+module.exports = router;
---
+++ service/apiV2DBService.js
@@ -0,0 +1,218 @@
+const {MongoClient} = require("mongodb");
+const config = require("../config");
+
+module.exports = {
+    projectionToken: {"_id": 0, tokenId:1, blockNumber:1, timestamp:1, value: 1,memo: 1, to: 1, holder: "$to",
+        tokenIndex: "$token.tokenIndex", quantity: "$token.quantity", royalties: "$token.royalties",
+        royaltyOwner: "$token.royaltyOwner", createTime: '$token.createTime', tokenIdHex: '$token.tokenIdHex',
+        name: "$token.name", description: "$token.description", kind: "$token.kind", type: "$token.type",
+        thumbnail: "$token.thumbnail", asset: "$token.asset", size: "$token.size", tokenDid: "$token.did",
+        adult: "$token.adult"},
+
+    queryCollectibleByTokenId: async function(tokenId) {
+        let client = new MongoClient(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
+        try {
+            await client.connect();
+            let collection = client.db(config.dbName).collection('pasar_token_event');
+
+            let result = await collection.aggregate([
+                { $match: {tokenId}},
+                { $sort: {tokenId: 1, blockNumber: -1}},
+                { $group: {_id: "$tokenId", doc: {$first: "$$ROOT"}}},
+                { $replaceRoot: { newRoot: "$doc"}},
+                { $lookup: {from: "pasar_token", localField: "tokenId", foreignField: "tokenId", as: "token"} },
+                { $unwind: "$token"},
+                { $project: this.projectionToken}
+            ]).toArray();
+
+            return {code: 200, message: 'success', data: result[0]};
+        } catch (err) {
+            logger.error(err);
+            return {code: 500, message: 'server error'};
+        } finally {
+            await client.close();
+        }
+    },
+
+    queryCollectibleByOther: async function(types, keyword) {
+        let client = new MongoClient(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
+        try {
+            await client.connect();
+            let collection = client.db(config.dbName).collection('pasar_token_event');
+
+            let match = {};
+            if(types === 'creator') {
+                match["token.royaltyOwner"] = keyword;
+            } else if(types === 'owner') {
+                match["to"] = keyword;
+            } else if(types === 'keyword') {
+                match = {$or: [{"token.name": {$regex: keyword}}, {"token.description": {$regex: keyword}}]}
+            }
+
+            let result = await collection.aggregate([
+                { $sort: {tokenId: 1, blockNumber: -1}},
+                { $group: {_id: "$tokenId", doc: {$first: "$$ROOT"}}},
+                { $replaceRoot: { newRoot: "$doc"}},
+                { $lookup: {from: "pasar_token", localField: "tokenId", foreignField: "tokenId", as: "token"} },
+                { $unwind: "$token"},
+                { $match: match},
+                { $project: this.projectionToken}
+            ]).toArray();
+
+            return {code: 200, message: 'success', data: result};
+        } catch (err) {
+            logger.error(err);
+            return {code: 500, message: 'server error'};
+        } finally {
+            await client.close();
+        }
+    },
+
+    queryAllCollectibles: async function() {
+        let client = new MongoClient(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
+        try {
+            await client.connect();
+            let collection = client.db(config.dbName).collection('pasar_token_event');
+
+            let result = await collection.aggregate([
+                { $sort: {tokenId: 1, blockNumber: -1}},
+                { $group: {_id: "$tokenId", doc: {$first: "$$ROOT"}}},
+                { $replaceRoot: { newRoot: "$doc"}},
+                { $lookup: {from: "pasar_token", localField: "tokenId", foreignField: "tokenId", as: "token"} },
+                { $unwind: "$token"},
+                { $project: this.projectionToken}
+            ]).toArray();
+
+            return {code: 200, message: 'success', data: result};
+        } catch (err) {
+            logger.error(err);
+            return {code: 500, message: 'server error'};
+        } finally {
+            await client.close();
+        }
+    },
+
+    queryAllCreators: async function() {
+        let client = new MongoClient(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
+        try {
+            await client.connect();
+            let collection = client.db(config.dbName).collection('pasar_token');
+
+            let result = await collection.distinct("royaltyOwner");
+
+            return {code: 200, message: 'success', data: result};
+        } catch (err) {
+            logger.error(err);
+            return {code: 500, message: 'server error'};
+        } finally {
+            await client.close();
+        }
+    },
+
+    /**
+     * @param orderState 1 => orderForSale  2 => orderFilled  3 => orderCanceled
+     * @param types tokenId  buyer
+     * @param keyword
+     * @returns {Promise<{code: number, message: string}|{code: number, data: *, message: string}>}
+     */
+    queryOrders: async function(orderState, types, keyword) {
+        let client = new MongoClient(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
+        try {
+            await client.connect();
+            let collection = client.db(config.dbName).collection('pasar_order');
+
+            let query = {orderState, [types]: keyword}
+
+            let result = await collection.find(query).projection({"_id": 0}).toArray();
+
+            return {code: 200, message: 'success', data: result};
+        } catch (err) {
+            logger.error(err);
+            return {code: 500, message: 'server error'};
+        } finally {
+            await client.close();
+        }
+    },
+
+    projectionOrder: {"_id": 0, to: 1, seller: "$to", orderId: "$order.orderId", amount: "$order.amount",bids: "$order.bids",
+        blockNumber: "$order.blockNumber",buyerAddr: "$order.buyerAddr",createTime: "$order.createTime",endTime: "$order.endTime",
+        filled: "$order.filled",lastBid: "$order.lastBid",lastBidder: "$order.lastBidder",orderState: "$order.orderState",
+        orderType: "$order.orderType",price: "$order.price",royaltyFee: "$order.royaltyFee",royaltyOwner: "$order.royaltyOwner",
+        sellerAddr: "$order.sellerAddr",tokenId: "$order.tokenId",updateTime: "$order.updateTime"},
+
+    queryOrdersBySeller: async function(orderState, seller) {
+        let client = new MongoClient(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
+        try {
+            await client.connect();
+            let collection = client.db(config.dbName).collection('pasar_token_event');
+
+            let query = {orderState, [types]: keyword}
+
+            let result = await collection.aggregate([
+                { $sort: {tokenId: 1, blockNumber: -1}},
+                { $group: {_id: "$tokenId", doc: {$first: "$$ROOT"}}},
+                { $replaceRoot: { newRoot: "$doc"}},
+                { $lookup: {from: "pasar_order", localField: "tokenId", foreignField: "tokenId", as: "order"} },
+                { $unwind: "$order"},
+                { $match: {to: seller, "order.orderState": orderState}},
+                { $project: this.projectionOrder}
+            ]).toArray();
+
+            return {code: 200, message: 'success', data: result};
+        } catch (err) {
+            logger.error(err);
+            return {code: 500, message: 'server error'};
+        } finally {
+            await client.close();
+        }
+    },
+
+    // {kind: "$token.kind", type: "$token.type", size: "$token.size",
+    // royalties: "$token.royalties",royaltyOwner: "$token.royaltyOwner", quantity: "$token.quantity",
+    // tokenDid: "$token.did", thumbnail: "$token.thumbnail", tokenCreateTime: "$token.createTime",
+    // tokenUpdateTime: "$token.updateTime", adult: "$token.adult"}
+    queryOrdersByKeyword: async function(orderState, keyword) {
+        let client = new MongoClient(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
+        try {
+            await client.connect();
+            let collection = client.db(config.dbName).collection('pasar_order');
+
+            let result = await collection.aggregate([
+                { $match: {orderState}},
+                { $lookup: {from: "pasar_token", localField: "tokenId", foreignField: "tokenId", as: "token"} },
+                { $unwind: "$token"},
+                { $match: {$or: [{"token.name": {$regex: keyword}}, {"token.description": {$regex: keyword}}]}},
+                { $project: {"_id": 0, orderId:1, orderType:1, orderState:1, tokenId: 1,blockNumber: 1, amount: 1,
+                        price: 1, endTime: 1, sellerAddr: 1, buyerAddr: 1, bids: 1, lastBidder: 1, filled:1, royaltyFee: 1,
+                        createTime: 1, updateTime: 1, lastBid: 1,sellerDid: 1, asset: "$token.asset", name: "$token.name",
+                        description: "$token.description"}}
+            ]).toArray();
+
+            return {code: 200, message: 'success', data: result};
+        } catch (err) {
+            logger.error(err);
+            return {code: 500, message: 'server error'};
+        } finally {
+            await client.close();
+        }
+    },
+
+    queryAllOrder: async function(orderState) {
+        let client = new MongoClient(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
+        try {
+            await client.connect();
+            let collection = client.db(config.dbName).collection('pasar_order');
+
+            let query = orderState !== undefined ? {orderState} : {}
+
+            let result = await collection.find(query).projection({"_id": 0}).toArray();
+
+            return {code: 200, message: 'success', data: result};
+        } catch (err) {
+            logger.error(err);
+            return {code: 500, message: 'server error'};
+        } finally {
+            await client.close();
+        }
+    },
+}
