00e290bdcf5d75734cac9c4920f280bd2eee5531
LiFayi <lifayi2008@gmail.com>
2021-12-17 16:36:03 +0800

    bug fix

--- check/checkTokenHolder.js
+++ check/checkTokenHolder.js
@@ -6,7 +6,36 @@ async function checkTokenHolder() {
     try {
         await mongoClient.connect();
         const collection = mongoClient.db(config.dbName).collection('pasar_token');
-        return await collection.find({}).sort({blockNumber: -1}).project({"_id": 0, tokenId: 1, holder: 1}).toArray();
+        let result = await collection.find({}).sort({blockNumber: -1}).project({"_id": 0, tokenId: 1, holder: 1}).toArray();
+
+        let tokenHolderMap = new Map();
+        result.forEach(item => {
+            tokenHolderMap.set(item.tokenId, item.holder);
+        })
+
+        const collection2 = mongoClient.db(config.dbName).collection('pasar_token_event');
+        let result2 = await collection2.aggregate([
+            { $sort: {blockNumber: -1}},
+            { $group: {_id: "$tokenId", doc: {$first: "$$ROOT"}}},
+            { $replaceRoot: { newRoot: "$doc"}},
+            { $project: {"_id": 0, tokenId:1,  holder: "$to"}}
+        ]).toArray();
+
+        let tokenEventHolderMap = new Map();
+        result2.forEach(item => {
+            tokenEventHolderMap.set(item.tokenId, item.holder);
+        })
+
+        let i = 0;
+        tokenEventHolderMap.forEach((holder, tokenId) => {
+            if(holder !== tokenHolderMap.get(tokenId)) {
+                console.log(`${tokenId}:\t${tokenHolderMap.get(tokenId)} <==> ${holder}`)
+                i++;
+            }
+        })
+        console.log(`Token Holder Count:\t${tokenHolderMap.size}`);
+        console.log(`Token Event Holder Count:\t${tokenEventHolderMap.size}`);
+        return i;
     } catch (err) {
         logger.error(err);
     } finally {
@@ -14,45 +43,5 @@ async function checkTokenHolder() {
     }
 }
 
-checkTokenHolder().then(async result => {
-
-    console.log(`pasar_token: ${result.length}`)
-    let a1 = new Map();
-    result.forEach(item => {
-        a1.set(item.tokenId, item.holder);
-    })
-
-    let client = new MongoClient(config.mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
-    await client.connect();
-    let collection = client.db(config.dbName).collection('pasar_token_event');
-
-    let result2 = await collection.aggregate([
-        { $sort: {blockNumber: -1}},
-        { $group: {_id: "$tokenId", doc: {$first: "$$ROOT"}}},
-        { $replaceRoot: { newRoot: "$doc"}},
-        { $project: {"_id": 0, tokenId:1,  holder: "$to"}}
-    ]).toArray();
-
-    console.log(`pasar_token_event: ${result2.length}`)
-    let a2 = new Map();
-    result2.forEach(item => {
-        a2.set(item.tokenId, item.holder);
-    })
-
-    let i = 0;
-    a2.forEach((value, key) => {
-        if(a1.get(key) === undefined) {
-            console.log(`${key} not in pasar_token`)
-
-        }
-        // else {
-        //     if(value !== a1.get(key)) {
-        //         i++;
-        //         console.log(`${key}:  ${value} == ${a1.get(key)}`)
-        //     }
-        // }
-    })
-
-    // console.log(i)
-})
+checkTokenHolder().then(console.log)
 
